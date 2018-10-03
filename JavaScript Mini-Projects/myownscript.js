a349add062a20872c1ba085aa0e21410ea4af4a9
galindo4926 <43216040+galindo4926@users.noreply.github.com>
2018-10-02 20:47:00 -0500

    third upload twice

--- JavaScript Mini-Projects/myownscript.js
+++ JavaScript Mini-Projects/myownscript.js
@@ -3,4 +3,33 @@
  */
 
 function degrees()
-var Temperature=number
+var degrees =
+
+
+
+/*
+MINI Project 3
+TASK: Write a function named 'assignGrade' that:
+    -Takes 1 argument, a numeric score
+-Return a grade for the score, either A, B, C, D, or F+
+-Call that function for a few different scores and log the result.
+    HTML:
+() Form HTML
+() Input field
+() Button
+() Result field
+JavaScript:
+    () See 'HelloWorld' code, but the with numbers
+*/
+function assignGrade()
+{
+    //capture a score from HTML
+    var score = Number(document.project3.num1.value);
+    if (score >= 90) //that's an A
+    {
+        return 'A';
+    }
+    else if (score >= 80)
+    {
+        return 'B';
+    }  //FINISH IT!!! MORTAL KOMBAT!
